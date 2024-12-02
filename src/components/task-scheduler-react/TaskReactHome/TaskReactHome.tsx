501f1b352f02a2fd8b9c27b6bba07b49ec44a288
Priyanshu Sharma <84620013+PriyanshuCK@users.noreply.github.com>
2024-12-02 19:40:41 +0530

    Remove Chakra UI from the project

--- src/components/task-scheduler-react/TaskReactHome/TaskReactHome.tsx
+++ src/components/task-scheduler-react/TaskReactHome/TaskReactHome.tsx
@@ -1,12 +1,11 @@
 import React, { FC } from "react";
 import "./TaskReactHome.css";
-// import { Input } from "@chakra-ui/react";
 
 interface TaskReactHomeProps {}
 
 const TaskReactHome: FC<TaskReactHomeProps> = () => (
   <>
-    {/* <Input placeholder="What's on your mind, today?" variant="flushed" />{" "} */}
+    <h1>Task Scheduler</h1>
   </>
 );
 
--- src/components/task-scheduler-react/TaskSchedulerReact/TaskSchedulerReact.tsx
+++ src/components/task-scheduler-react/TaskSchedulerReact/TaskSchedulerReact.tsx
@@ -1,15 +1,12 @@
 import React, { FC } from "react";
 import "./TaskSchedulerReact.css";
 import TaskReactHome from "../TaskReactHome/TaskReactHome";
-// import { Provider } from "../../ui/provider";
 
 interface TaskSchedulerReactProps {}
 
 const TaskSchedulerReact: FC<TaskSchedulerReactProps> = () => (
   <>
-    {/* <Provider> */}
     <TaskReactHome />
-    {/* </Provider> */}
   </>
 );
 
